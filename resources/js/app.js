50e277a8cfa7fbcdebbd8c555e27e8136ca5975c
muchron <msfiqih@gmail.com>
2025-01-06 15:04:26 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import 'flowbite';
+import 'flowbite-datepicker/DateRangePicker';
+import 'flowbite-datepicker/Datepicker';
+import 'remixicon/fonts/remixicon.css'
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+const colors = require('tailwindcss/colors');
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+
+    darkMode: 'class',
+    theme: {
+        // colors: {
+        //     gray: colors.grau,
+        //     blue: colors.sky,
+        //     red: colors.rose,
+        //     pink: colors.fuchsia,
+        // },
+        fontFamily: {
+            sans: ['Graphik', 'sans-serif'],
+            serif: ['Merriweather', 'serif'],
+        },
+        // extend: {
+        //     spacing: {
+        //         '128': '32rem',
+        //         '144': '36rem',
+        //     },
+        //     borderRadius: {
+        //         '4xl': '2rem',
+        //     }
+        // }
+    },
+    plugins: [('flowbite/plugin')],
+};
