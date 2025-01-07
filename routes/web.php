f2aac3f03fee1fdb1fb5ee0702d1b3a58554121c
muchron <msfiqih@gmail.com>
2025-01-07 14:48:26 +0700

    page artikel

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.beranda');
 });
+Route::get('/artikel', function () {
+    return view('pages.artikel');
+});
