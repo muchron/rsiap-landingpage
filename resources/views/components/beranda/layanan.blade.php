e4d6f9c164fe2098c192e8ea4d0346b294898bc0
msfiqih <msfiqih@gmail.com>
2025-07-29 14:31:16 +0700

    add banner for RSIAP mobile

--- resources/views/components/beranda/layanan.blade.php
+++ resources/views/components/beranda/layanan.blade.php
@@ -57,3 +57,25 @@
 
     </div>
 </div>
+
+
+
+<section class="flex flex-col lg:flex-row min-h-[300px] max-h-[380px] overflow-hidden bg-green-100 dark:bg-gray-800 text-white relative">
+    <div class="w-full lg:w-1/2 p-8 lg:p-16 flex flex-col justify-center text-center lg:text-left h-full">
+        <h2 class="text-green-400 dark:text-green-300 text-2xl font-semibold mb-2">RSIAP Mobile</h2>
+        <h1 class="text-green-600 dark:text-green-400 text-4xl lg:text-5xl font-bold leading-tight mb-4">Daftar Periksa Lebih Mudah</h1>
+        <p class="text-gray-500 dark:text-white mb-8 max-w-lg mx-auto lg:mx-0">
+            Dengan RSIAP Mobile, periksa jadwal dokter, buat janji, dan dapatkan estimasi antrian sebelum mengunjungi. Kemudahan pelayanan kesehatan ada di tangan Anda.
+        </p>
+        <div class="flex flex-col sm:flex-row justify-center lg:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
+            <a href="#" class="inline-flex items-center justify-center bg-green-800 dark:bg-green-600 text-white px-6 py-3 rounded-lg shadow-md hover:bg-green-500 transition duration-300">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" alt="Google Play" class="h-8 mr-2">
+                Google Play
+            </a>
+        </div>
+    </div>
+
+    <div class="w-full lg:w-1/2 flex justify-end items-center h-full">
+        <img src="{{ asset('images/rsiap-mobile-mockup.png') }}" alt="Halo Hermina App on Phone" class="h-full w-full object-fill">
+    </div>
+</section>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('pages.beranda');
 });
+
+
 Route::get('/artikel', [App\Http\Controllers\Pages\Artikel::class, 'index'])->name('artikel');
 Route::get('/artikel/{slug}', [App\Http\Controllers\Pages\Artikel::class, 'get'])->name('artikel.read');
 
+Route::get('/test', function (ApiService $api) {
+    return $api->get('polyclinics');
+});
